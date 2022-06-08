1f96f62397f9b8e1f01740f8b3b652a5aae765da
kevin.guerrero <kevin.guerrero@newshore.es>
2022-06-07 22:30:20 -0500

    add implementation to swapDigits

--- src/swapdigits.js
+++ src/swapdigits.js
@@ -1,6 +1,26 @@
 const swapDigits = (number)=>{
     let swappedNumber = 0
     //write logic here
+    const stringNumber = number.toString().split('');
+
+    if (isNaN(number) || number <= 0) {
+        return 0;
+    }
+
+    if (stringNumber.length >= 2) {
+        if (stringNumber.length % 2 === 0) {
+            for(let i=1; i < stringNumber.length; i+=2 ) {
+                [stringNumber[i-1], stringNumber[i]] = [stringNumber[i], stringNumber[i-1]];
+            }
+        } else {
+            for(let i=2; i < stringNumber.length; i+=2 ) {
+                [stringNumber[i-1], stringNumber[i]] = [stringNumber[i], stringNumber[i-1]];
+            }
+        }
+        swappedNumber = parseInt(stringNumber.join(''));
+    } else {
+        swappedNumber = number;
+    }
     return swappedNumber;
     
 }
